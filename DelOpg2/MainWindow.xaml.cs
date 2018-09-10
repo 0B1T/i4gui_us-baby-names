2eef08e2853ea3ebb9d89f58ebe39a933e52e1eb
John Romby <au518762@uni.au.dk>
2018-09-10 17:38:57 +0200

    DelOpg2 added and made some initial progress in the exercise

---
+++ DelOpg2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace DelOpg1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnLoad(object sender, RoutedEventArgs ea)
+        {
+            string filename = @"..\..\..\DelOpg1\bin\Debug\05-babynames.txt";   // There must be a smarter way to do this!?
+            var decade = 1900;
+
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    // Reads the 10 first lines from the file and outputs them in the ListBox.
+                    for (int i = 0; i < 10; ++i)
+                    {
+                        firstDecadeTopNames.Items.Add(sr.ReadLine());
+                        decadesLB.Items.Add(decade);
+                        decade += 10;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.Write("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
